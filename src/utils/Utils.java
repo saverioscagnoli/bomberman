cf32aef7a1040570fbc0f82d8f97a70676c69685
Saverio <svscagn@gmail.com>
2023-05-15 20:56:56 +0200

    bomb explosion

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -4,7 +4,6 @@ import entities.GameCharacter;
 import ui.Button;
 
 public abstract class Utils {
-
     public static int[] normalizePos(int x, int y) {
         int gridX = ((int) (x - (x % Consts.tileDims)));
         int gridY = ((int) (y - (y % Consts.tileDims)));
@@ -20,5 +19,16 @@ public abstract class Utils {
 
     public static boolean buttonClick(int mouseX, int mouseY, Button btn) {
         return  ((mouseX >= btn.x && mouseX <= btn.x + btn.width) && (mouseY >= btn.y && mouseY <= btn.y + btn.height));
+    }
+
+    public static void setTimeout(Runnable runnable, int delay) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }).start();
     }
 }
